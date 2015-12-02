6269b391009bede632d890bf172a3891c4325629
zz <zhuguang@ejoy.com>
2015-12-02 14:42:34 +0800

    fix init

--- lua_wrap_gtxt.c
+++ lua_wrap_gtxt.c
@@ -52,7 +52,7 @@ linit(lua_State* L) {
 
 	gtxt_ft_init();
 
-	gtxt_glyph_cache_init(cap_bitmap, cap_layout);
+	gtxt_glyph_init(cap_bitmap, cap_layout, NULL);
 
 	gtxt_richtext_ext_sym_cb_init(&ext_sym_create, &ext_sym_release, &ext_sym_get_size, &ext_sym_render);
 
