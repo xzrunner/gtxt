0c98bc19026ec5cc2dbfa46600ee1fd17c7c3d8e
zz <zhuguang@ejoy.com>
2015-12-09 14:51:35 +0800

    [CHANGED] move richtext cb outside

--- lua_wrap_gtxt.c
+++ lua_wrap_gtxt.c
@@ -1,46 +1,11 @@
 #include "gtxt_adapter.h"
 #include "gtxt_freetype.h"
-#include "gtxt_glyph.h"
 #include "gtxt_richtext.h"
 
 #include <dtex_facade.h>
 
 #include <lua.h>
 #include <lauxlib.h>
-
-static void*
-ext_sym_create(const char* str) {
-	if (strncmp(str, "path=", 5) != 0) {
-		return NULL;
-	}
-
-	return NULL;
-}
-
-static void
-ext_sym_release(void* ext_sym) {
-	if (!ext_sym) {
-		return;
-	}
-}
-
-static void 
-ext_sym_get_size(void* ext_sym, int* width, int* height) {
-	if (!ext_sym) {
-		*width= *height = 0;
-		return;
-	}
-
-	*width = 0;
-	*height = 0;
-}
-
-static void
-ext_sym_render(void* ext_sym, float x, float y, void* ud) {
-	if (!ext_sym) {
-		return;
-	}
-}
 
 static int
 linit(lua_State* L) {
@@ -53,8 +18,6 @@ linit(lua_State* L) {
 	gtxt_ft_init();
 
 	gtxt_glyph_init(cap_bitmap, cap_layout, NULL);
-
-	gtxt_richtext_ext_sym_cb_init(&ext_sym_create, &ext_sym_release, &ext_sym_get_size, &ext_sym_render);
 
 	return 0;
 }
